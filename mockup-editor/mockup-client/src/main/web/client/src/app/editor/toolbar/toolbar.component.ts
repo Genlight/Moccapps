7999bd0afe4b8c02d24781a2c2c41ecfca3dc391
Yikai Yang <e1427424@student.tuwien.ac.at>
2019-04-09 21:42:56 +0200

    33569 - added toolbar icons, navbar refinement

--- mockup-editor/mockup-client/src/main/web/client/src/app/editor/toolbar/toolbar.component.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/editor/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faMousePointer, faFont, faLayerGroup, faThLarge } from '@fortawesome/free-solid-svg-icons';
+import { faHandPaper, faSquare, faCircle } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+
+  faMousePointer = faMousePointer;
+  faHandPointer = faHandPaper;
+  faSquare = faSquare;
+  faCircle = faCircle;
+  faFont = faFont;
+  faLayerGroup = faLayerGroup;
+  faThLarge = faThLarge;
 
   constructor() { }
 
