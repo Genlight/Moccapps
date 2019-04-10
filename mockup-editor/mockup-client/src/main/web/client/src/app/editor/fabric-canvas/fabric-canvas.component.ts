14a12a7d4f00e23165bbd16d6d8dbb3dde378de4
brigitteb <brigitteblank@gmail.com>
2019-04-10 23:29:12 +0200

    #34056 basic implementations of group, ungroup and delete with fabric.js

--- mockup-editor/mockup-client/src/main/web/client/src/app/editor/fabric-canvas/fabric-canvas.component.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/editor/fabric-canvas/fabric-canvas.component.ts
@@ -16,13 +16,13 @@ export class FabricCanvasComponent implements OnInit {
 
   ngOnInit() {
     this.canvas = new fabric.Canvas('canvas',
-    {
-      backgroundColor: '#ffffff'
-    });
-    this.setCanvasSize(400,400);
+      {
+        backgroundColor: '#ffffff'
+      });
+    this.setCanvasSize(400, 400);
   }
 
-  setCanvasSize(width, height){
+  setCanvasSize(width, height) {
     //TODO: validate input.
     this.canvas.setWidth(width);
     this.canvas.setHeight(height);
@@ -33,8 +33,43 @@ export class FabricCanvasComponent implements OnInit {
     this.canvas.add(label);
   }
 
-  onAddText(){
+  onAddText() {
     this.addText('Text');
+  }
+
+  onRemove() {
+    var actObj = this.canvas.getActiveObject();
+    this.canvas.remove(actObj);
+  }
+
+  onGroup() {
+    this.group();
+  }
+
+  onUngroup() {
+    this.ungroup();
+  }
+
+  ungroup() {
+    if (!this.canvas.getActiveObject()) {
+      return;
+    }
+    if (this.canvas.getActiveObject().type !== 'group') {
+      return;
+    }
+    this.canvas.getActiveObject().toActiveSelection();
+    this.canvas.requestRenderAll();
+  }
+
+  group() {
+    if (!this.canvas.getActiveObject()) {
+      return;
+    }
+    if (this.canvas.getActiveObject().type !== 'activeSelection') {
+      return;
+    }
+    this.canvas.getActiveObject().toGroup();
+    this.canvas.requestRenderAll();
   }
 
 }
