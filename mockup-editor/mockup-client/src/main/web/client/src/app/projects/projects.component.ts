badb24a6b8883fdd3978ad6237237005f72d998e
Yikai Yang <e1427424@student.tuwien.ac.at>
2019-04-21 19:23:20 +0200

    34049: Project overview page. Added button group toggles.

--- mockup-editor/mockup-client/src/main/web/client/src/app/projects/projects.component.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  user = {
+    username: 'Max Mustermann'
+  };
+
+  constructor(private router: Router) { 
+
+  }
 
   ngOnInit() {
   }
