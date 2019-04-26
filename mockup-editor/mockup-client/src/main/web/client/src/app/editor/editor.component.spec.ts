bd976f8944f574c43596648dd56e2543b8f5bba1
Alexander Genser <alex.genser@hotmail.com>
2019-04-26 03:38:24 +0200

    34819: WIP: trying to untie Unittests, Problems lie in project architecture -> everything is declared in appModule, but at least for the editor we should declare a separate module

--- mockup-editor/mockup-client/src/main/web/client/src/app/editor/editor.component.spec.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/editor/editor.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditorComponent } from './editor.component';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FabricCanvasComponent } from './fabric-canvas/fabric-canvas.component';
+import { faBars, faUndo, faRedo} from '@fortawesome/free-solid-svg-icons';
+import { faCommentAlt } from '@fortawesome/free-regular-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
 
 describe('EditorComponent', () => {
   let component: EditorComponent;
@@ -8,7 +15,12 @@ describe('EditorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditorComponent ]
+      declarations: [
+        EditorComponent,
+        FabricCanvasComponent,
+        ToolbarComponent,
+        NavbarComponent
+      ]
     })
     .compileComponents();
   }));
--- mockup-editor/mockup-client/src/main/web/client/src/app/editor/fabric-canvas/fabric-canvas.component.spec.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/editor/fabric-canvas/fabric-canvas.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FabricCanvasComponent } from './fabric-canvas.component';
+import { FabricmodifyService } from '../fabricmodify.service';
+import { ManagePagesService } from '../managepages.service';
+import { AsyncSubject } from 'rxjs/AsyncSubject';
+import { Observable } from 'rxjs';
+import { fabric } from '../extendedfabric';
+import {AppModule} from '../../app.module';
 
 describe('FabricCanvasComponent', () => {
   let component: FabricCanvasComponent;
@@ -8,7 +14,8 @@ describe('FabricCanvasComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FabricCanvasComponent ]
+      declarations: [ FabricCanvasComponent ],
+      providers: [ FabricmodifyService, ManagePagesService]
     })
     .compileComponents();
   }));
@@ -22,4 +29,35 @@ describe('FabricCanvasComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(': object should have a UUID', () => {
+    const obj = new  fabric.TextBox('textbox');
+    expect(obj.uuid).toEqual(jasmine.any(String));
+  });
+
+  it(': Transformations should be observable', () => {
+    let transformed = false;
+    component.Transformation.subscribe({next: () => { transformed = true; }});
+    component.onAddText();
+    expect(transformed).toBe(true);
+  });
+
+  it('should not emit Transformations on applytransformations', () => {
+    let transformed = false;
+    component.Transformation.subscribe({next: () => { transformed = true; }});
+    const rect = new fabric.Rect();
+    component.applyTransformation(rect);
+    expect(transformed).toBe(false);
+  });
+  it('should not emit Transformations on applyRemoval', () => {
+    let transformed = false;
+    component.onAddText();
+    component.Transformation.subscribe({next: () => { transformed = true; }});
+    const rect = new fabric.Rect();
+    component.applyRemoval(rect);
+    expect(transformed).toBe(false);
+  });
+  // it('should have called ManagePageService', () => {
+  //   expect(pageService.createPage).toHaveBeenCalled();
+  // });
 });
