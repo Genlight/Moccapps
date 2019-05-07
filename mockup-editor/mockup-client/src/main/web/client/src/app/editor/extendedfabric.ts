f25587051ad2a3804e1d1752338a7680e13a6fc6
brigitteb <brigitteblank@gmail.com>
2019-05-07 15:26:51 +0200

    #36259: - made circle and rectangle bigger so the render stroke issue isn't as visible for now - overrode renderstroke method from fabric.js to reapply the stroke after transformation and the getTransformationDimensions function for correctly displaying the bounding box #36183: - added form validation for change project name modal

--- mockup-editor/mockup-client/src/main/web/client/src/app/editor/extendedfabric.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/editor/extendedfabric.ts
@@ -31,4 +31,77 @@ fabric.Object.prototype.initialize = function(options) {
 export { fabric };
 
 
+/** 
+ * overrides renderStroke to redraw the border of objects on resize,
+ * so the border doesn't get scaled in x and y direction
+ * based on https://stackoverflow.com/questions/39548747/fabricjs-how-to-scale-object-but-keep-the-border-stroke-width-fixed
+ * TODO: still reverts back to original stroke when object is grouped 
+ */
+fabric.Object.prototype._renderStroke = function(ctx) {
+    if (!this.stroke || this.strokeWidth === 0) {
+        return;
+    }
+    if (this.shadow && !this.shadow.affectStroke) {
+        this._removeShadow(ctx);
+    }
+    ctx.save();
+    if (this.group) {
+        ctx.scale(1 / this.group.scaleX, 1 / this.group.scaleY);
+    } else {
+        ctx.scale(1 / this.scaleX, 1 / this.scaleY);
+    }
+    this._setLineDash(ctx, this.strokeDashArray, this._renderDashedStroke);
+    this._applyPatternGradientTransform(ctx, this.stroke);
+    ctx.stroke();
+    ctx.restore();
+};
 
+/** 
+ * overrides getTransformationDimensions to get the correct bounding box
+ * with the changes made to renderStroke
+ * based on https://stackoverflow.com/questions/39548747/fabricjs-how-to-scale-object-but-keep-the-border-stroke-width-fixed
+ */
+fabric.Object.prototype._getTransformedDimensions = function(skewX, skewY) {
+    if (typeof skewX === 'undefined') {
+        skewX = this.skewX;
+    }
+    if (typeof skewY === 'undefined') {
+        skewY = this.skewY;
+    }
+    const dimX = this.width / 2;
+    const dimY = this.height / 2;
+
+    const points = [{
+        x: -dimX,
+        y: -dimY
+    }, {
+        x: dimX,
+        y: -dimY
+    }, {
+        x: -dimX,
+        y: dimY
+    }, {
+        x: dimX,
+        y: dimY
+    }];
+    const transformMatrix = this._calcDimensionsTransformMatrix(skewX, skewY, false);
+    for (let i = 0; i < points.length; i++) {
+        points[i] = fabric.util.transformPoint(points[i], transformMatrix);
+    }
+    const bbox = fabric.util.makeBoundingBoxFromPoints(points);
+    return {
+        y: bbox.height + this.strokeWidth,
+        x: bbox.width + this.strokeWidth,
+    };
+};
+
+fabric.Object.prototype.resizeToScale = function () {
+    if  (this.type !=='group') {
+      this.strokeWidth = this._origStrokeWidth / Math.max(this.scaleX, this.scaleY);
+    } else {
+      this._objects.forEach( function(obj) {
+        console.log(obj);
+        obj.strokeWidth = obj._origStrokeWidth / Math.max(obj.group.scaleX, obj.group.scaleY);
+      });
+    }
+  };
