f5f834b6350d4864a867925e68085e7a7f929473
AlexG <alex.genser@hotmail.com>
2019-05-08 16:22:50 +0200

    36456: FIXED: notifications are shown instead of alerts

--- mockup-editor/mockup-client/src/main/web/client/src/app/shared/components/user-modal/user-modal.component.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/shared/components/user-modal/user-modal.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-// import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { User } from '../../models/User';
 import { UserinfoService } from './userinfo.service';
 import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { AuthService } from '../../../auth/auth.service';
 import { AuthLogoutInfo } from '../../../auth/logout-info';
-
+import { NotificationService } from '../../services/notification.service';
 @Component({
   selector: 'app-user-modal',
   templateUrl: './user-modal.component.html',
@@ -21,6 +20,7 @@ export class UserModalComponent implements OnInit {
     public activeModal: NgbActiveModal,
     private userInfoService: UserinfoService,
     private router: Router,
+    private notificationService: NotificationService,
     private authService: AuthService
   ) { }
 
@@ -39,20 +39,23 @@ export class UserModalComponent implements OnInit {
           if (this.user.pwd !== '') {
               this.authService.logout(new AuthLogoutInfo(this.user.email)).subscribe(
                 () => {
-                  alert('Success on Update. Because you changed your Password, you will be logged out. Pleas Sign in again.');
+                  this.notificationService.showSuccess(
+                    `Because you changed your Password, you will be logged out. Pleas Sign in again.`,
+                    'Success on Update'
+                  );
                 }
               );
           } else {
-            alert('success onUpdateUserInfo');
+            this.notificationService.showSuccess('Update successful');
             this.router.navigate(['editor']);
           }
         } else {
-          alert('Fail at onUpdateUserInfo, s. Console Output');
+          this.notificationService.showError('Fail at onUpdateUserInfo, s. Console Output');
           this.activeModal.close('error');
         }
       },
       () => {
-        alert('error');
+        this.notificationService.showError('error');
         this.activeModal.close('error');
       },
       () => {
