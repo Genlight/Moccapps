beb85bb70c35f5bba24f8d68ef502484279999c1
Alex G <alex.genser@hotmail.com>
2019-05-10 13:21:37 +0000

    Update .gitlab-ci.yml, mockup-editor/mockup-client/src/main/web/client/src/karma.conf.js files

--- mockup-editor/mockup-client/src/main/web/client/src/karma.conf.js
+++ mockup-editor/mockup-client/src/main/web/client/src/karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+//process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
@@ -28,11 +28,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome_no_sandbox'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
-      Chrome_no_sandbox: {
+      ChromeHeadless: {
         base: 'Chrome',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox', 'headless', 'disable-gpu']
       }
     },
     singleRun: true,
