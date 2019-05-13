4e730b3d9d68be62178b39305b576d5274ce2952
AlexG <alex.genser@hotmail.com>
2019-05-13 23:29:59 +0200

    WIP: Test for ci

--- mockup-editor/mockup-server/src/main/java/ase/springboot/Application.java
+++ mockup-editor/mockup-server/src/main/java/ase/springboot/Application.java
@@ -22,7 +22,7 @@ import javax.validation.Valid;
 @PropertySource(value = "classpath:application.properties")
 public class Application extends SpringBootServletInitializer {
 
-	@Value("${spring.datasource.url.test:jdbc:postgresql://localhost:5432/test}")
+	@Value("${spring.datasource.url.test}") // removed default :jdbc:postgresql://localhost:5432/test
 	public String testDataUrl;
 
 
