b8e52836e21c63ab98c149c3e85c32fec79cd40a
Alex G <alex.genser@hotmail.com>
2019-05-15 18:36:52 +0000

    36798: WIP: updating Restservice for pwd validation

--- mockup-editor/mockup-client/src/main/web/client/src/app/shared/components/user-modal/user-modal.component.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/shared/components/user-modal/user-modal.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { User } from '../../models/User';
+import { Password } from '../../models/Password';
 import { UserinfoService } from './userinfo.service';
 import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { AuthService } from '../../../auth/auth.service';
 import { AuthLogoutInfo } from '../../../auth/logout-info';
 import { NotificationService } from '../../services/notification.service';
+// import { EqualValidator } from '../../directives/equalValidator.directive';
+
 @Component({
   selector: 'app-user-modal',
   templateUrl: './user-modal.component.html',
   styleUrls: ['./user-modal.component.scss']
+  // directives: [EqualValidator]
 })
 export class UserModalComponent implements OnInit {
 
   user: User;
-
+  pwd: Password;
   constructor(
     public activeModal: NgbActiveModal,
     private userInfoService: UserinfoService,
@@ -26,6 +30,7 @@ export class UserModalComponent implements OnInit {
 
   ngOnInit() {
     this.user = new User();
+    this.pwd = new Password();
     this.userInfoService.getUserInfo()
       .subscribe((data: any) => this.user = {
         name:  data.name,
@@ -33,10 +38,10 @@ export class UserModalComponent implements OnInit {
     });
   }
   onUpdateUserInfo(): void {
-    this.userInfoService.updateUserInfo(this.user).subscribe(
+    this.userInfoService.updateUserInfo(this.user, this.pwd).subscribe(
       (data: any) => {
         if (data.message === 'success') {
-          if (this.user.pwd !== '') {
+          if (this.pwd.pwd !== '') {
                 this.notificationService.showSuccess(
                   `Because you changed your Password, you will be logged out. Pleas Sign in again.`,
                   'Success on Update'
@@ -47,12 +52,12 @@ export class UserModalComponent implements OnInit {
             this.router.navigate(['editor']);
           }
         } else {
-          this.notificationService.showError('Fail at onUpdateUserInfo, s. Console Output');
+          this.notificationService.showError(data.message);
           this.activeModal.close('error');
         }
       },
       () => {
-        this.notificationService.showError('error');
+        this.notificationService.showError('Error');
         this.activeModal.close('error');
       },
       () => {
