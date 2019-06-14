e3a4350524e0d9edf88cf7efbb5abf23fd7e532e
maisi <maisi1415@gmail.com>
2019-06-14 14:28:39 +0200

    Add comment tests

--- mockup-editor/mockup-server/src/test/resources/schema.sql
+++ mockup-editor/mockup-server/src/test/resources/schema.sql
@@ -3,6 +3,9 @@ CREATE SEQUENCE IF NOT EXISTS seq_project;
 CREATE SEQUENCE IF NOT EXISTS seq_page;
 CREATE SEQUENCE IF NOT EXISTS seq_user_project;
 CREATE SEQUENCE IF NOT EXISTS seq_invitation;
+CREATE SEQUENCE IF NOT EXISTS seq_comment;
+CREATE SEQUENCE IF NOT EXISTS seq_comment_entry;
+CREATE SEQUENCE IF NOT EXISTS seq_comment_object;
 
 CREATE TABLE IF NOT EXISTS users
 (
@@ -51,7 +54,33 @@ CREATE TABLE IF NOT EXISTS invitations
   FOREIGN KEY (inviter_user_id) REFERENCES users (id)  ON DELETE CASCADE,
   FOREIGN KEY (invitee_user_id) REFERENCES users (id)  ON DELETE CASCADE,
   FOREIGN KEY (project_id) REFERENCES projects (id)  ON DELETE CASCADE
-
-
 );
+
+CREATE TABLE IF NOT EXISTS comments
+(
+  id        INTEGER DEFAULT nextval('seq_comment') PRIMARY KEY,
+  page_id   INTEGER       NOT NULL,
+  cleared   BOOLEAN,
+  FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS comment_objects
+(
+  id        INTEGER DEFAULT nextval('seq_comment_object') PRIMARY KEY,
+  comment_id   INTEGER       NOT NULL,
+  object_id VARCHAR(100)  NOT NULL,
+  FOREIGN KEY (comment_id) REFERENCES comments (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS comment_entries
+(
+  id        INTEGER DEFAULT nextval('seq_comment_entry') PRIMARY KEY,
+  message   VARCHAR(1000) NOT NULL,
+  user_id   INTEGER       NOT NULL,
+  comment_id INTEGER NOT NULL,
+  date      DATE          NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (comment_id) REFERENCES comments  (id) ON DELETE CASCADE
+);
+
 
