29ae2965bf44ccbb2813ffb324132d8009a02c38
baermat <bernhard.matyas@gmx.at>
2019-06-16 00:17:27 +0200

    Fixed transmission and application of objects that are in groups and active selections. in detail, fixed:  - moving, rotating, scaling of actively selected elements  - dynamic active selection in response to these changes  - application of changes from one party another has actively selected

--- mockup-editor/mockup-client/src/main/web/client/src/app/editor/fabricmodify.service.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/editor/fabricmodify.service.ts
@@ -254,36 +254,67 @@ export class FabricmodifyService {
       const old = this.getObjectByUUID(parsedObj.uuid, canvas);
     //console.log('test: applyTransformation' + ', parsedObj: ' + parsedObj + ', sendMe: ' + parsedObj.sendMe + ', parsedObjuuid: ' + parsedObj.uuid + ', retrievedObj: ' + old + ', JSONmessage:' + JSON.stringify(message));
 
-    console.log('pre enlivenment: ' + JSON.stringify(parsedObj));
-    fabric.util.enlivenObjects([parsedObj], function (objects) {
-      objects.forEach(function (o) {
+    //console.log('pre enlivenment: ' + JSON.stringify(parsedObj));
+    //fabric.util.enlivenObjects([parsedObj], function (objects) {
+      //objects.forEach(function (o) {
 
-        console.log('after enlivenment: ' + JSON.stringify(o));
-          o.uuid = parsedObj.uuid;
+        //console.log('after enlivenment: ' + JSON.stringify(o));
+          //o.uuid = parsedObj.uuid;
 
           if (message.command === Action.ADDED) {
             //old exists if I created the object myself. clean solution: make add button send change but not add in the first place
             //for 99.9% of the cases, this will suffice (0.1%: in case of uuid collision this becomes inconsistent)
             if(!old) {
-            o.sendMe = false;
-            canvas.add(o);
+            //o.sendMe = false;
+            //canvas.add(o);
+            this.addRemoteObject(parsedObj,canvas);
             }
           }
           else if (message.command === Action.MODIFIED) {
 
             //fallback to add if no such element exists, can be removed and replaced by error message if desired
             if (old === undefined) {
-              o.sendMe = false;
-              canvas.add(o);
+              //o.sendMe = false;
+              //canvas.add(o);
+              this.addRemoteObject(parsedObj,canvas);
             } else {
-              let activeSelection = canvas.getActiveObjects();
-              console.log('contains test\nselection: ' + JSON.stringify(activeSelection) + '\nobject: ' + o.uuid);
-              console.log('\ncontain result: ' + activeSelection.indexOf(o));
-              let keys = Object.keys(o);
+              let selectionChange:boolean = false;
+              let activeSelection = canvas.getActiveObject();
+              let positionInCurrentSelection:number;
+              if(activeSelection && activeSelection.type === 'activeSelection') {
+                let activeSelectionObjects = canvas.getActiveObjects();
+
+                console.log('contains test\nselection: ' + JSON.stringify(activeSelectionObjects) + '\nobject: ' + parsedObj.uuid);
+                console.log('\ncontain result: ' + activeSelectionObjects.indexOf(old));
+                 positionInCurrentSelection = activeSelectionObjects.indexOf(old);
+
+                if(positionInCurrentSelection !== -1) {
+
+                  selectionChange=true;
+                  FabricmodifyService.extractFromGroup(old,activeSelection);
+                  //canvas.renderOnAddRemove = false;
+
+                  //activeSelection.remove(old);
+                  
+              //old.setCoords();
+              //console.log('modified selection: ' +JSON.stringify(activeSelection));
+                }
+              }
+              
+              let keys = Object.keys(parsedObj);
+              
+              
               keys.forEach(function (key) {
                 //console.log(`assigning ${o[key]} to ${key}, old value: ${old[key]}`)
-                old[key] = o[key];
+                old[key] = parsedObj[key];
               });
+              old.sendMe = true;
+              if(selectionChange) {
+                //activeSelection.insertAt(old, positionInCurrentSelection);
+                //canvas.renderOnAddRemove = true;
+                FabricmodifyService.insertIntoGroup(old, activeSelection);
+              }
+
               //this is necessary to reliably render all changes of the object
               old.setCoords();
             }
@@ -296,9 +327,9 @@ export class FabricmodifyService {
             }
           }
         
-      });
+      //});
 
-    });
+    //});
     console.log('after parse.');
   }
   canvas.renderAll();
@@ -308,5 +339,38 @@ export class FabricmodifyService {
   getObjectByUUID(uuid: string, canvas: any) {
     //this.canvas = this.managePagesService.getCanvas(); //commented as managePageService was removed, needs testing
     return canvas.getObjects().find((o) => o.uuid === uuid);
+  }
+  private static extractFromGroup(obj,group) {
+    console.log('pre extract obj mod: ' +JSON.stringify(obj));
+    let groupHeightMiddle:number = group.height / 2.0;
+    let groupWidthMiddle:number = group.width /2.0;
+    let groupLeft:number = group.left;
+    let groupTop:number = group.top;
+
+    obj.left = groupLeft + (groupWidthMiddle+obj.left);
+    obj.top = groupTop + (groupHeightMiddle+obj.top);
+    
+    console.log('post extract obj mod: ' +JSON.stringify(obj));
+  }
+  private static insertIntoGroup(obj,group) {
+    
+    console.log('pre insert obj mod: ' +JSON.stringify(obj));
+    let groupHeightMiddle:number = group.height / 2.0;
+    let groupWidthMiddle:number = group.width /2.0;
+    let groupLeft:number = group.left;
+    let groupTop:number = group.top;
+    obj.left = obj.left - (groupLeft+groupWidthMiddle);
+    obj.top = obj.top - (groupTop+groupHeightMiddle);
+    
+    console.log('post insert mod: ' +JSON.stringify(obj));
+  }
+
+  private addRemoteObject(obj, canvas) {
+    fabric.util.enlivenObjects([obj], function (objects) {
+      objects.forEach(function (o) {
+        o.uuid = obj.uuid;
+        canvas.add(o);
+      });
+    });
   }
 }
