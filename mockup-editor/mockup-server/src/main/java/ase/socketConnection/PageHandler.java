c36e3a77ac8cdec6a9518172acc968aa68a92a85
Matthias Deimel <e1427725@student.tuwien.ac.at>
2019-06-16 15:23:40 +0200

    34048: add page commands

--- mockup-editor/mockup-server/src/main/java/ase/socketConnection/PageHandler.java
+++ mockup-editor/mockup-server/src/main/java/ase/socketConnection/PageHandler.java
@@ -1,7 +1,6 @@
 package ase.socketConnection;
 
 import ase.DTO.Page;
-import ase.DTO.User;
 import ase.message.socket.SocketMessage;
 import ase.service.PageService;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -10,15 +9,12 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import springfox.documentation.spring.web.json.Json;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+
 
 public class PageHandler {
 
@@ -26,21 +22,23 @@ public class PageHandler {
     private Page page;
     private PageService pageService;
     private ObjectMapper objectMapper;
+    private String projectId;
 
     private static final Logger logger= LoggerFactory.getLogger(PageHandler.class);
 
 
-    public PageHandler(int pageId, PageService pageService){
+    public PageHandler(String projectId,int pageId, PageService pageService){
         currentUser=new ArrayList<>();
         objectMapper=new ObjectMapper();
         this.pageService = pageService;
         page=pageService.getPageById(pageId);
+        this.projectId=projectId;
     }
 
     public boolean handleMessage(SocketMessage message){
         JsonNode pageData;
-        logger.info("Command: " + message.getCommand());
-        logger.info("Content: " + message.getContent());
+        logger.debug("Command: " + message.getCommand());
+        logger.debug("Content: " + message.getContent());
         try {
             pageData = objectMapper.readTree(page.getPage_data());
         } catch (IOException e) {
@@ -103,6 +101,48 @@ public class PageHandler {
                     logger.error("coudln't parse content in element:removed");
                     return false;
                 }
+            case "page:modified":{
+                try {
+                    ObjectNode content = objectMapper.readValue(message.getContent(),ObjectNode.class);
+                    Iterator contentIt=content.fieldNames();
+
+                    ObjectNode pageDataOn=pageData.deepCopy();
+                    while( contentIt.hasNext()){
+                        String fieldName =(String) contentIt.next();
+                        if(fieldName.equals("objects")){
+                            continue;
+                        }
+                        String value = content.get(fieldName).asText();
+                        pageDataOn.put(fieldName,content.get(fieldName).asText());
+                    }
+                    page.setPage_data(pageDataOn.toString());
+                } catch (IOException e) {
+                    logger.error("coudln't parse content in page:modified");
+                    return false;
+                }
+                return true;
+            }
+            case "page:dimensionchange":{
+                try {
+                    ObjectNode content = objectMapper.readValue(message.getContent(),ObjectNode.class);
+                    page.setHeight(content.get("changeheight").asInt());
+                    page.setWidth(content.get("changewidth").asInt());
+                    return true;
+                } catch (IOException e) {
+                    logger.error("coudln't parse content in page:dimensionchange");
+                    return false;
+                }
+            }
+            case "page:renamed":{
+                try {
+                    ObjectNode content = objectMapper.readValue(message.getContent(),ObjectNode.class);
+                    page.setPage_name(content.get("pageName").asText());
+                    return true;
+                } catch (IOException e) {
+                    logger.error("coudln't parse content in page:renamed");
+                    return false;
+                }
+            }
         }
         return true;
     }
@@ -131,9 +171,17 @@ public class PageHandler {
         return currentUser;
     }
 
+    public String getPageData(){
+        return page.getPage_data();
+    }
+
     private void shutdown(){
         pageService.update(page);
         logger.debug("page: " + page.getId() + " persisted");
+    }
+
+    public String getProjectId() {
+        return projectId;
     }
 
 }
--- mockup-editor/mockup-server/src/main/java/ase/socketConnection/SocketServer.java
+++ mockup-editor/mockup-server/src/main/java/ase/socketConnection/SocketServer.java
@@ -15,25 +15,20 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 @Controller
 public class SocketServer {
-    //UserId , Queue to answer
-    private Map<String, LinkedBlockingQueue<SocketMessage>> messageQueues;
+    //Current Users mapped to Id
+    private Map<String,UserHandler> currentUser;
     //SessionID, UserId
     private Map<String, String> sessionIdUserIdMap;
     //PageID, pageHandler of current active pages
     private Map<String, PageHandler> pageHandlerMap;
-    //UserID,PageID
-    private Map<String,String> userPageMap;
-    //UserID,ProjectID
-    private Map<String,String> userProjectMap;
+
     private ExecutorService executorService;
     private SocketConnectionHandler connectionHandler;
 
@@ -46,68 +41,111 @@ public class SocketServer {
     private PageService pageService;
 
     public SocketServer(){
-        messageQueues=Collections.synchronizedMap(new HashMap<>());
         executorService= Executors.newCachedThreadPool();
         sessionIdUserIdMap =Collections.synchronizedMap(new HashMap<>());
         pageHandlerMap =new HashMap<>();
-        userPageMap=new HashMap<>();
-        userProjectMap=new HashMap<>();
+
+        currentUser=new HashMap<>();
     }
 
     @EventListener
     public void handleWebSocketSubscribeListener(SessionSubscribeEvent sessionSubscribeEvent){
-        logger.info(sessionSubscribeEvent.getMessage().toString());
         String destination=(String) sessionSubscribeEvent.getMessage().getHeaders().get("simpDestination");
         String sessionId=(String) sessionSubscribeEvent.getMessage().getHeaders().get("simpSessionId");
+
         if(!destination.matches("/user/.*/queue/send")){
             return;
         }
+
         LinkedBlockingQueue<SocketMessage> queue=new LinkedBlockingQueue<>();
-        destination=destination.split("/")[2];
-        connectionHandler=new SocketConnectionHandler(queue,destination,messagingTemplate);
-        messageQueues.put(destination,queue);
-        sessionIdUserIdMap.put(sessionId,destination);
+        String userId=destination.split("/")[2];
+
+        UserHandler userHandler=new UserHandler();
+        userHandler.setResponderQueue(queue);
+        userHandler.setUserId(userId);
+        currentUser.put(userId,userHandler);
+
+        sessionIdUserIdMap.put(sessionId,userId);
+
+        connectionHandler=new SocketConnectionHandler(queue,userId,messagingTemplate);
         executorService.submit(connectionHandler);
     }
 
     @EventListener
-    public void handleWebSocketDisconnectListener(SessionDisconnectEvent sessionDisconnectEvent){
+    public void onDisconnectEvent(SessionDisconnectEvent sessionDisconnectEvent){
         String sessionId=(String) sessionDisconnectEvent.getMessage().getHeaders().get("simpSessionId");
         String userId= sessionIdUserIdMap.get(sessionId);
+        String pageId=currentUser.get(userId).getPageId();
         sessionIdUserIdMap.remove(sessionId);
-        if(!pageHandlerMap.get(userPageMap.get(userId)).removeUser(userId)){
-            pageHandlerMap.remove(userPageMap.get(userId));
+        if(!pageHandlerMap.get(pageId).removeUser(userId)){
+            pageHandlerMap.remove(pageId);
         }
-        userPageMap.remove(userId);
-        userProjectMap.remove(userId);
         try {
-            messageQueues.get(userId).put(new SocketMessage("","",userId,"disconnect",""));
-            messageQueues.remove(userId);
+            currentUser.get(userId).getResponderQueue().put(new SocketMessage("","",userId,"disconnect",""));
         } catch (InterruptedException e) {
             logger.error("Client Socket Connection Handler Thread is already closed");
         }
+
+        currentUser.remove(userId);
     }
 
     @MessageMapping("/send")
     public void onReceive(@Payload SocketMessage message) throws IOException {
-        logger.info("MESSAGE: " + message.toString());
+        logger.debug("MESSAGE: " + message.toString());
         if(!pageHandlerMap.keySet().contains(message.getPageId())){
             String pageIdString = message.getPageId();
             int pageId = Integer.parseInt(pageIdString);
-            PageHandler pageHandler = new PageHandler(pageId, pageService);
+
+            PageHandler pageHandler = new PageHandler(message.getProjectId(), pageId, pageService);
             pageHandlerMap.put(pageIdString, pageHandler);
-            userPageMap.put(message.getUser(),message.getPageId());
-            userProjectMap.put(message.getUser(),message.getProjectId());
+            UserHandler handler = currentUser.get(message.getUser());
+
+            handler.setProjectId(message.getProjectId());
+            handler.setPageId(message.getPageId());
         }
         pageHandlerMap.get(message.getPageId()).addUser(message.getUser());
-        pageHandlerMap.get(message.getPageId()).handleMessage(message);
+        UserHandler user= currentUser.get(message.getUser());
+        switch (message.getContent()){
+            case "page:load":
+                try {
+                    user.getResponderQueue().put(new SocketMessage(
+                            message.getProjectId(),
+                            message.getPageId(),
+                            "server",
+                            "page:content",
+                            pageHandlerMap.get(message.getPageId()).getPageData()));
+                } catch (InterruptedException e) {
+                    logger.error("Can not communicate with Client Socket Connection Handler Thread during Pageload");
+                }
+                break;
+            case "page:created":
+                break;
+            default:
+                if (!pageHandlerMap.get(message.getPageId()).handleMessage(message)){
+                    return;
+                }
+                break;
+        }
 
-        for(String userId:pageHandlerMap.get(message.getPageId()).getUser()){
+        if(message.getCommand().startsWith("page")){
+            for(PageHandler pageHandler:pageHandlerMap.values()){
+                if(pageHandler.getProjectId().equals(message.getProjectId())){
+                    sendMessages(pageHandler.getUser(),message);
+                }
+            }
+            return;
+        }
+        sendMessages(pageHandlerMap.get(message.getPageId()).getUser(),message);
+    }
+
+    private void sendMessages(List<String> user, SocketMessage message){
+        for(String userId:user){
             try {
-                messageQueues.get(userId).put(message);
+                currentUser.get(userId).getResponderQueue().put(message);
             } catch (InterruptedException e) {
                 logger.error("Can not communicate with Client Socket Connection Handler Thread");
             }
         }
     }
+
 }
