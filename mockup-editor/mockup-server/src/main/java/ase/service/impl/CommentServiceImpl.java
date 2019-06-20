d6362dafecb65402321947cd19ac2467b937fa58
maisi <maisi1415@gmail.com>
2019-06-20 12:39:52 +0200

    40459: Small changes to tests

--- mockup-editor/mockup-server/src/main/java/ase/service/impl/CommentServiceImpl.java
+++ mockup-editor/mockup-server/src/main/java/ase/service/impl/CommentServiceImpl.java
@@ -9,10 +9,12 @@ import org.mapstruct.ObjectFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CommentServiceImpl implements CommentService {
 
     private static final Logger logger = LoggerFactory.getLogger(CommentServiceImpl.class);
