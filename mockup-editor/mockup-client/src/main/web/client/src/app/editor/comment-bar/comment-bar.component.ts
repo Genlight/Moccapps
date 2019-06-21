fa1ca9ab79f20b898dcf27f6c528171cafd67878
Yikai Yang <e1427424@student.tuwien.ac.at>
2019-06-21 02:45:49 +0200

    40459: finishing comments ui

--- mockup-editor/mockup-client/src/main/web/client/src/app/editor/comment-bar/comment-bar.component.ts
+++ mockup-editor/mockup-client/src/main/web/client/src/app/editor/comment-bar/comment-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Comment, CommentEntry} from '../../shared/models/comments';
 import { CommentService } from '../comment.service';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { WorkspaceService } from '../workspace.service';
 
 @Component({
   selector: 'app-comment-bar',
@@ -12,14 +13,20 @@ export class CommentBarComponent implements OnInit {
   comments: Comment[];
 
   newComment: string;
+
+  showsComponent: boolean;
   addingNewComment;
+  isLoading: boolean = false; 
 
   faTimes = faTimes;
 
-  constructor(private commentService: CommentService) {}
+  constructor(private commentService: CommentService, private workspaceService: WorkspaceService) {}
 
   ngOnInit() {
     this.initCommentservice();
+    this.workspaceService.showsComments.subscribe((value) => {
+      this.showsComponent = value;
+    });
   }
 
   initCommentservice() {
@@ -42,7 +49,6 @@ export class CommentBarComponent implements OnInit {
   // creating a comment on an exisiting
   onCreateComment() {
     this.addingNewComment = true;
-
   }
 
   onAddComment() {
@@ -50,6 +56,11 @@ export class CommentBarComponent implements OnInit {
     this.newComment = '';
     this.addingNewComment = false;
   }
+
+  onClose() {
+    this.workspaceService.hideComments();
+  }
+
   /**
    * test
    * @return void
